6178b631d5c9d397cb5ddd59149aeb4e42f4871a
Galen Rice <griceturrble@protonmail.com>
2025-01-20 22:50:12 -0500

    feat: sync command; docstring fixes

--- src/discord_bot_template/__init__.py
+++ src/discord_bot_template/__init__.py
@@ -16,25 +16,40 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    await bot.tree.sync(guild=discord.Object(id=DISCORD_GUILD))
+    """Let us know we logged in properly."""
     print(f"Logged in as {bot.user} (ID: {bot.user.id})")
 
 
 @bot.tree.command(
-    description="Replies to /hello",
+    description="Replies with Hello!",
     guild=discord.Object(id=DISCORD_GUILD),
 )
 async def hello(interaction: discord.Interaction):
     """Just say hello."""
     print("Responding to /hello")
-    await interaction.response.send_message("Hello, how's it going?")
+    await interaction.response.send_message("Hello, how are you?")
 
 
-@bot.command(description="Replies to !whatsup")
+@bot.command(
+    description="Replies to !whatsup",
+    guild=discord.Object(id=DISCORD_GUILD),
+)
 async def whatsup(ctx):
     """Tell us what's up."""
     print("Responding to !whatsup")
     await ctx.send("Nothing much")
+
+
+@bot.command(
+    description="Sync the bot tree to update Slash command changes to the server",
+    guild=discord.Object(id=DISCORD_GUILD),
+)
+async def sync(ctx):
+    """Sync the bot command tree for Slash commands."""
+    print("Syncing the command tree...")
+    await bot.tree.sync(guild=discord.Object(id=DISCORD_GUILD))
+    await ctx.reply("✅ Command tree synced")
+    print("Sync Complete!")
 
 
 def main():
